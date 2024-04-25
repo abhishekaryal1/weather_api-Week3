3155959205ef7b596b3394c3abaa73daec7c788f
Abhishek Aryal <153663851+abhishekaryal1@users.noreply.github.com>
2024-04-25 19:38:49 +0000

    Blockchain integration

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,1 +1,17 @@
-// your config code
+import algosdk from "algosdk";
+
+const algodToken = "a".repeat(64);
+const server: string ="http:/localhost";
+const port:string = "4001";
+
+const mnemonic: string = "push brave joke either basic punch fragile where bottom example mistake lake knee skull head more merry gap school arch gift friend certain abandon dad"
+
+export function getClient(): algosdk.Algodv2 {
+    let client = new algosdk.Algodv2(algodToken, server, port);
+    return client;
+}
+
+export function getAccount(): algosdk.Account {
+    let account = algosdk.mnemonicToSecretKey(mnemonic);
+    return account;
+}
--- src/helpers/helper.ts
+++ src/helpers/helper.ts
@@ -1,1 +1,29 @@
-// your helper code
+import algosdk from "algosdk";
+import { getClient, getAccount } from "../config/config.js";
+
+export const storeWeatherData = async (data: WeatherData): Promise<void> => {
+    try {
+        const client = getClient();
+        const account = getAccount();
+        const suggestedParams = await client.getTransactionParams().do();
+
+        const note = algosdk.encodeObj(data);
+
+        const txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+            from: account.addr,
+            to: account.addr,
+            amount: 1000,
+            note: note,
+            suggestedParams: suggestedParams,
+        });
+
+        const signedTxn = txn.signTxn(account.sk);
+        const sendTxn = await client.sendRawTransaction(signedTxn).do();
+
+        console.log("Transaction ID:", sendTxn.txId);
+    }
+    catch (error) {
+        console.error("Failed to store weather data:", error);
+
+    }
+};
